711f622f4e54447a8fff23df3d869bc832e144f1
Leonardo G N Henrique <leonardognh@outlook.com.br>
2025-07-09 16:37:14 -0300

    barrels

--- rollup.config.cjs
+++ rollup.config.cjs
@@ -1,28 +1,50 @@
 // rollup.config.cjs
 const typescript = require("rollup-plugin-typescript2");
 const pkg = require("./package.json");
+const peers = Object.keys(pkg.peerDependencies || {});
 
-const externals = Object.keys(pkg.peerDependencies || {});
-const globals = {
-  mitt: "mitt",
-  react: "React",
-  "@angular/core": "ng.core",
-  "@angular/common": "ng.common",
-};
+module.exports = [
+  // 1) Multi-entry para CJS e ES (code splitting)
+  {
+    input: {
+      index: "src/index.ts",
+      angular: "src/angular/index.ts",
+      react: "src/react/index.ts",
+    },
+    external: peers,
+    output: [
+      {
+        dir: "dist",
+        entryFileNames: "[name]/index.cjs.js",
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        dir: "dist",
+        entryFileNames: "[name]/index.esm.js",
+        format: "es",
+        sourcemap: true,
+      },
+    ],
+    plugins: [typescript({ tsconfig: "./tsconfig.json" })],
+  },
 
-module.exports = {
-  input: "src/index.ts",
-  external: externals,
-  output: [
-    { file: pkg.main, format: "cjs", sourcemap: true },
-    { file: pkg.module, format: "es", sourcemap: true },
-    {
-      file: pkg.browser,
+  // 2) Single-entry para UMD (apenas root)
+  {
+    input: "src/index.ts",
+    external: peers,
+    output: {
+      file: "dist/index.umd.js",
       format: "umd",
       name: "EventBus",
-      globals,
+      globals: {
+        mitt: "mitt",
+        react: "React",
+        "@angular/core": "ng.core",
+        "@angular/common": "ng.common",
+      },
       sourcemap: true,
     },
-  ],
-  plugins: [typescript({ tsconfig: "./tsconfig.json" })],
-};
+    plugins: [typescript({ tsconfig: "./tsconfig.json" })],
+  },
+];
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,1 @@
-// Core
 export { emit, on, off } from "./bus";
-
-// Angular
-export * from "./angular/event-bus.service";
-
-// React
-export * from "./react/useEventBus";
-export * from "./react/useEmit";
