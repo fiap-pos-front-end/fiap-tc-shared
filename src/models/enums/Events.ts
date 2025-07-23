42bfaeee08bc7983b669f80ab6d987d34574d169
João Vitor Veronese Vieira <joaovitorvv@gmail.com>
2025-07-23 18:11:58 -0300

    feat: add new event

--- src/models/enums/Events.ts
+++ src/models/enums/Events.ts
@@ -2,4 +2,5 @@ export const EVENTS = {
   USER_LOGGED_IN: 'user-logged-in',
   USER_LOGGED_OUT: 'user-logged-out',
   TRANSACTIONS_UPDATED: 'transactions-updated',
+  BALANCE_UPDATED: 'balance-updated',
 };
