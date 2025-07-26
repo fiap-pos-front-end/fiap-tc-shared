768e9141c2292c5691d56df0ba2de447f88c35c4
Leonardo G N Henrique <leonardognh@outlook.com.br>
2025-07-26 19:13:45 -0300

    fix: filenames

---
+++ src/models/enums/events.ts
@@ -0,0 +1,6 @@
+export const EVENTS = {
+  USER_LOGGED_IN: "user-logged-in",
+  USER_LOGGED_OUT: "user-logged-out",
+  TRANSACTIONS_UPDATED: "transactions-updated",
+  BALANCE_UPDATED: "balance-updated",
+};
